edcb92923c016e1ea227d31d3dff9e1dc85ad0fc
oloendithas <oloendithas@gmail.com>
2014-07-24 10:54:02 +0400

    Initial commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 2 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#2 SMP PREEMPT Thu Jul 24 10:48:03 MSK 2014"
+#define LINUX_COMPILE_BY "arthur"
+#define LINUX_COMPILE_HOST "arthur-virtual-machine"
+#define LINUX_COMPILER "gcc version 4.6 20120106 (prerelease) (GCC) "
