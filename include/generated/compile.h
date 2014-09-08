984470f8be76f9d12178d5aca80652fe06d201c8
oloendithas <oloendithas@gmail.com>
2014-09-08 22:30:19 +0400

    just some Make stuff

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 156 */
+/* This file is auto generated, version 362 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#156 SMP PREEMPT Fri Aug 1 19:14:32 MSK 2014"
+#define UTS_VERSION "#362 SMP PREEMPT Mon Sep 8 22:26:37 MSK 2014"
 #define LINUX_COMPILE_BY "arthur"
 #define LINUX_COMPILE_HOST "arthur-virtual-machine"
-#define LINUX_COMPILER "gcc version 4.6 20120106 (prerelease) (GCC) "
+#define LINUX_COMPILER "gcc version 4.7.4 20140402 (prerelease) (crosstool-NG linaro-1.13.1+bzr2651 - Linaro GCC 2014.04) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.4.39-g0153552-dirty"
+#define UTS_RELEASE "3.4.39-gf78ea32-dirty"
