fa1e777aa0a79ae18a8df20c579f006b9e890655
oloendithas <oloendithas@gmail.com>
2014-10-02 05:52:08 +0400

    kernel/sched/fair.c: fix merge derp

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4534,7 +4534,7 @@ redo:
 		env.load_move	= imbalance;
 		env.src_cpu	= busiest->cpu;
 		env.src_rq	= busiest;
-		env.loop_max	= min(sysctl_sched_nr_migrate, busiest->nr_running);
+		env.loop_max	= min_t(unsigned int, sysctl_sched_nr_migrate, busiest->nr_running);
 
 more_balance:
 		local_irq_save(flags);
